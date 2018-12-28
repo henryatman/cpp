aaebbdc9bb375ac6ddcc4f2d108dab940eab3014
王弘毅 <v@varsoft.net>
2018-12-28 11:32:10 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2018年12月28日
+ *      Author: altman
+ */
+
+#include "TicketMachine.h"
+
+int main()
+{
+	TicketMachine tm;
+	tm.insertMoney(100);
+	tm.showBalance();
+	return 0;
+}
+
+
